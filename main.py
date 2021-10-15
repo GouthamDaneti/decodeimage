898ed3b9dd41580d16bc5d1396d5c7049e2d627a
GouthamDaneti <gouthamdaneti@gmail.com>
2021-10-15 16:32:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,40 @@
 def main():
     #print("Please enter the input here!")
     #print('yo 001')
+    isFirstLine = True
     while True:
     	currNumLines = int(input())
     	if (currNumLines == 0):
     		break
     	#print(currNumLines)
     	#print('yo 002')
-    	printTheSeq(currNumLines)
+    	printTheSeq(currNumLines, isFirstLine)
+	isFirstLine = False
 
-def printTheSeq(numOfLine):
+def printTheSeq(numOfLine, isFirstLine):
 	compArr = []
 	#print('yo 003')
 	for x in range(numOfLine):
 		char = input()
 		compArr.append(char)
-	printSeq(compArr)
+	printSeq(compArr, isFirstLine)
 	if (validate(compArr) != True):
 		print('Error decoding image!')
 		
 
-def printSeq(compArr):
-	first = True
+def printSeq(compArr, isFirstLine):
+	if not isFirstLine:
+		print("\n", end='')
 	for line in compArr:
 		#print(line)
 		data = line.split()
 		char = data.pop(0)
-		if not first:
-        		print("\n", end='')
-		first = False
 		for x in data:
 			printCurrChar(char, x)
 			if (char == '#'):
 				char = '.'
 			else:
 				char = '#'
-	print("\n", end='')
 	print("\n", end='')
 	#print("\n", end='')
 
